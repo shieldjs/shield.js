8ec4277cfdda70b5db49a01355f86718239e1110
Devin Rhode <devinrhode2@gmail.com>
2013-04-02 10:46:16 -0700

    Update shield.js

--- src/shield.js
+++ src/shield.js
@@ -19,121 +19,115 @@ function wrapInTryCatch(fn) {
   };
 }
 
-(function shieldJS() {
-  'use strict';
+/**
 
-  /**
+## The main `shield` function does a few things:<br/>
+
+1) Wraps a callback in a try/catch block:
+
+  shield(function(){
+    //your program
+  })();
+
+2) Optionally include a `console` param to use our historicalConsole
+
+  shield(function(console){
+  })();
+
+For documentation regarding keeping a console history, see
+<a href="https://github.com/devinrhode2/historicalConsole.js">github.com/devinrhode2/historicalConsole.js</a>
+
+<br>
+3) Modify global api functions so their callbacks are also wrapped in a try/catch block:
+
+  shield('$');
+  shield('$, $.fn.on, $.fn.ready')
   
-  ## The main `shield` function does a few things:<br/>
-  
-  1) Wraps a callback in a try/catch block:
+  //Now errors in these callbacks will be caught, and there's no need for a try/catch block:
+  $(function(){})
+  $('#foo').on('click', function(){})
+  $(document).ready(function(){})
 
-    shield(function(){
-      //your program
-    })();
+4) Use it for easier try/catch blocks. Instead of:
 
-  2) Optionally include a `console` param to use our historicalConsole
+  var func = function() {
+    //your function
+  };
 
-    shield(function(console){
-    })();
+  add shield:
+  var func = shield(function(){
+    //no need for a try/catch block now, shield has it taken care of
+  });
 
-  For documentation regarding keeping a console history, see
-  <a href="https://github.com/devinrhode2/historicalConsole.js">github.com/devinrhode2/historicalConsole.js</a>
-
-  <br>
-  3) Modify global api functions so their callbacks are also wrapped in a try/catch block:
-
-    shield('$');
-    shield('$, $.fn.on, $.fn.ready')
-    
-    //Now errors in these callbacks will be caught, and there's no need for a try/catch block:
-    $(function(){})
-    $('#foo').on('click', function(){})
-    $(document).ready(function(){})
-
-  4) Use it for easier try/catch blocks. Instead of:
-
-    var func = function() {
-      //your function
-    };
-
-    add shield:
-    var func = shield(function(){
-      //no need for a try/catch block now, shield has it taken care of
-    });
-
-##### You do not invoke this function with `new` (I think it's just a memory suck if you do)
+### You do not invoke this function with `new` (I think it's just a memory suck if you do)
 
 
-  @class shield
-  @constructor shield
-  @type Function
-  @param apiFn {String || Function} A string must represent a global function like `'$'`, or a space/comma-space seperated list like `'$, $.fn.ready'` <br>
-    Pass in a function, and it will be shieled and returned. `shield`'ing means this function and callbacks
-    passed as parameters to it will have all exceptions sent to onuncaughtError, or shield subscribers. (example 4)
-  @param {String} [promises] Space or comma-space separated list of promise functions to shield (like $.ajax().done)
-  @return {Function} A function that will have all uncaught exceptions sent to your shield.js subscribers, or onuncaughtException if you overwrote the function. You shield.unsubscribe(shield.report)
-  */
-  function shield(apiFn, promises) {
-    if (_.isString(apiFn)) {
-      if (apiFn.indexOf(' ') > -1) {
-        apiFn.replace(/\,/g, ''); //allow '$, $.fn.on, $.fn.ready'
-        apiFn = apiFn.split(' ');
-      }
+@class shield
+@constructor shield
+@type Function
+@param apiFn {String || Function} A string must represent a global function like `'$'`, or a space/comma-space seperated list like `'$, $.fn.ready'` <br>
+  Pass in a function, and it will be shieled and returned. `shield`'ing means this function and callbacks
+  passed as parameters to it will have all exceptions sent to onuncaughtError, or shield subscribers. (example 4)
+@param {String} [promises] Space or comma-space separated list of promise functions to shield (like $.ajax().done)
+@return {Function} A function that will have all uncaught exceptions sent to your shield.js subscribers, or onuncaughtException if you overwrote the function. You shield.unsubscribe(shield.report)
+*/
+function shield(apiFn, promises) {
+  if (_.isString(apiFn)) {
+    if (apiFn.indexOf(' ') > -1) {
+      apiFn.replace(/\,/g, ''); //allow '$, $.fn.on, $.fn.ready'
+      apiFn = apiFn.split(' ');
     }
-    if (_.isArray(apiFn)) {
-      _.each(apiFn, function(api){
-        shield(api);
-      });
-      return;
-    }
-    return wrapInTryCatch(extendFunction(apiFn, function(args, prevFunc) {
-      apiFn = null;//garbage collected
-
-      //if function.length (number of listed parameters) is 1, and there are no args, then this is
-      //shield(function(console){})()
-      //ie, prevFunc expects 1 arg (length) but received none when called
-      if (prevFunc.length === 1 && args.length === 0) {
-
-        //verify a 'console' param
-        var prevFnString = prevFunc.toString();
-        var firstParen = prevFnString.indexOf('(');
-        var secondParen = prevFnString.indexOf(')', firstParen);
-        if (prevFnString.substring(firstParen, secondParen).indexOf('console') > -1 && typeof historicalConsole !== 'undefined') {
-          //historicalConsole takes in a function and returns one that will receive the first arg as the console.
-          //The second arg is a unique identifier to use another scope's historical console object
-          //options.url is probably a deent unique identifier.
-          //We could ask the user to name the app (shield.options.appName('thing')
-          return historicalConsole(prevFunc/*, options.url*/);
-        } else {
-          return prevFunc;
-        }
-      } else {
-        //instead of just doing apiFn.apply, we interate through args
-        //and if an arg is a function then we wrap then we swap that fn for callback in a try/catch
-        var length = args.length;
-        //before executing the overriden function, transform each function arg to have a try/catch wrapper
-        //I'd prefer to keep the while/length style iteration here for performance, since this can be rather important
-        var arg;
-        while (arg = args[--length]) {
-          if (_.isFunction(arg)) {
-            arg = wrapInTryCatch(arg);
-          }
-        }
-
-        //now we apply the modified arguments:
-        var ret = prevFunc.apply(this, args);
-        if (promises) {
-          promises = promises.split(' ');
-          var promise;
-          while(promise = promises.pop()) {
-            ret[promise] = shield(ret[promise]);
-          }
-        }
-        return ret;
-      }
-    }));
   }
+  if (_.isArray(apiFn)) {
+    _.each(apiFn, function(api){
+      shield(api);
+    });
+    return;
+  }
+  return wrapInTryCatch(extendFunction(apiFn, function(args, prevFunc) {
+    apiFn = null;//garbage collected
 
-  window.shield = shield;
-})();
+    //if function.length (number of listed parameters) is 1, and there are no args, then this is
+    //shield(function(console){})()
+    //ie, prevFunc expects 1 arg (length) but received none when called
+    if (prevFunc.length === 1 && args.length === 0) {
+
+      //verify a 'console' param
+      var prevFnString = prevFunc.toString();
+      var firstParen = prevFnString.indexOf('(');
+      var secondParen = prevFnString.indexOf(')', firstParen);
+      if (prevFnString.substring(firstParen, secondParen).indexOf('console') > -1 && typeof historicalConsole !== 'undefined') {
+        //historicalConsole takes in a function and returns one that will receive the first arg as the console.
+        //The second arg is a unique identifier to use another scope's historical console object
+        //options.url is probably a deent unique identifier.
+        //We could ask the user to name the app (shield.options.appName('thing')
+        return historicalConsole(prevFunc/*, options.url*/);
+      } else {
+        return prevFunc;
+      }
+    } else {
+      //instead of just doing apiFn.apply, we interate through args
+      //and if an arg is a function then we wrap then we swap that fn for callback in a try/catch
+      var length = args.length;
+      //before executing the overriden function, transform each function arg to have a try/catch wrapper
+      //I'd prefer to keep the while/length style iteration here for performance, since this can be rather important
+      var arg;
+      while (arg = args[--length]) {
+        if (_.isFunction(arg)) {
+          arg = wrapInTryCatch(arg);
+        }
+      }
+
+      //now we apply the modified arguments:
+      var ret = prevFunc.apply(this, args);
+      if (promises) {
+        promises = promises.split(' ');
+        var promise;
+        while(promise = promises.pop()) {
+          ret[promise] = shield(ret[promise]);
+        }
+      }
+      return ret;
+    }
+  }));
+}
