5031fc47adb8ff5143da019a5895c5b773d972cb
Devin Rhode <devinrhode2@gmail.com>
2013-12-17 06:14:28 -0600

    export library as AMD module lodash style

--- src/shield.js
+++ src/shield.js
@@ -201,5 +201,52 @@ var func = shield(function(){
   shield.report = shield_report;
   shield.noConflict = shield_noConflict;
 
-  window.shield = shield;
-})(this);
+  // Export/define library just like lodash
+
+  /** Used to determine if values are of the language type Object */
+  var objectTypes = {
+    'function': true,
+    'object': true
+  };
+
+  /** Used as a reference to the global object */
+  var root = (objectTypes[typeof window] && window) || this;
+
+  /** Detect free variable `exports` */
+  var freeExports = objectTypes[typeof exports] && exports && !exports.nodeType && exports;
+
+  /** Detect free variable `module` */
+  var freeModule = objectTypes[typeof module] && module && !module.nodeType && module;
+
+  /** Detect the popular CommonJS extension `module.exports` */
+  var moduleExports = freeModule && freeModule.exports === freeExports && freeExports;
+
+  /** Detect free variable `global` from Node.js or Browserified code and use it as `root` */
+  var freeGlobal = objectTypes[typeof global] && global;
+  if (freeGlobal && (freeGlobal.global === freeGlobal || freeGlobal.window === freeGlobal)) {
+    root = freeGlobal;
+  }
+
+  // some AMD build optimizers like r.js check for condition patterns like the following:
+  if (typeof define == 'function' && typeof define.amd == 'object' && define.amd) {
+    // define as a named module like jQuery and underscore.js
+    define("shield", [], function () {
+      return shield;
+    });
+  }
+  // check for `exports` after `define` in case a build optimizer adds an `exports` object
+  else if (freeExports && freeModule) {
+    // in Node.js or RingoJS
+    if (moduleExports) {
+      freeModule.exports = shield;
+    }
+    // in Narwhal or Rhino -require
+    else {
+      freeExports.shield = shield;
+    }
+  }
+  else {
+    // in a browser or Rhino
+    root.shield = shield;
+  }
+}).call(this);
